2a89c403524a33507477de3f9a8369dfc59bc1c6
Evan Jeffries <ejeffries14@gmail.com>
2019-09-08 05:54:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+     @word = word
+     
+  end
+  
+  def match(array)
+    array.collect do |words|
+      words.split("").sort == @word.split("").sort
+    end
+  end
+end
 # Your code goes here!
